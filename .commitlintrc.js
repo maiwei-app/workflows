4d1a120115ab42975b865f63299bcf0762c74023
F Colomer <colomr@pm.me>
2026-08-15 21:52:37 +0200

    chore: include maibot-app trailers in commitlint ignore

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -4,8 +4,8 @@ module.exports = {
     // raise body max line length so release-please messages don't fail
     'body-max-line-length': [2, 'always', 200],
   },
-  // ignore Release Please related messages and customizations
+  // ignore Release Please related messages, customizations, and own bot trailers
   ignores: [
-    (commit) => /Release Please|release-please/i.test(commit),
+    (commit) => /Release Please|release-please|Co-authored-by: maibot-app/i.test(commit),
   ],
 };
